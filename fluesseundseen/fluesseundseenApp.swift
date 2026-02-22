c453e3f2f4bb6ac2adf72c0a415fb913f077ed2d
Antonio Aestero <aesterostudios@icloud.com>
2026-02-22 19:08:12 +0100

    Initial Commit

---
+++ fluesseundseen/fluesseundseenApp.swift
@@ -0,0 +1,17 @@
+//
+//  fluesseundseenApp.swift
+//  fluesseundseen
+//
+//  Created by Antonio Baltic on 22.02.26.
+//
+
+import SwiftUI
+
+@main
+struct fluesseundseenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
